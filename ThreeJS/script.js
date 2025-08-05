8d0229e0e36854aac4cdcfb11f8ef348fd91b85c
Joao Victor <joctor.carvalho2004@gmail.com>
2025-08-04 23:45:01 -0300

    feat: iniciando estudos typescript

--- ThreeJS/script.js
+++ ThreeJS/script.js
@@ -2,10 +2,10 @@ import * as THREE from "three";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
 );
 
 // const camera = new THREE.PerspectiveCamera(FOV==75deg, PROPORÇÃO==16:9|4:3, PLANO DE RECORTE==Proximo 0.1, PLANO DE RECORTE==Distante 1000);
@@ -24,12 +24,12 @@ scene.add(cube);
 camera.position.z = 5;
 
 function animate() {
-  requestAnimationFrame(animate);
+    requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.02;
-  cube.rotation.y += 0.02;
+    cube.rotation.x += 0.02;
+    cube.rotation.y += 0.02;
 
-  renderer.render(scene, camera);
+    renderer.render(scene, camera);
 }
 
 animate();
