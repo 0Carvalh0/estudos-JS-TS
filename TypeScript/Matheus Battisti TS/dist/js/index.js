34f5639ed00d009b05c1fb0e331fdf7b9365a9a4
Joao Victor <joctor.carvalho2004@gmail.com>
2025-08-06 15:55:57 -0300

    feat: finalizado intensivao typescript matheus Battisti

--- TypeScript/Matheus Battisti TS/dist/js/index.js
+++ TypeScript/Matheus Battisti TS/dist/js/index.js
@@ -1,5 +1,155 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 let x = 10;
 x = 5;
 console.log(x);
+let y = 20;
+let nome = "Joao";
+let age = 21;
+const isAdmin = true;
+console.log(typeof nome);
+const myNumbers = [1, 2, 3];
+console.log(myNumbers);
+console.log(myNumbers.length);
+myNumbers.push(5);
+console.log(myNumbers);
+let myTupla;
+myTupla = [5, "Joao", ["a", "b", "c"]];
+console.log(myTupla);
+const user = {
+    name: "Joao",
+    age: 21,
+};
+console.log(user);
+console.log(user.name);
+let a = 0;
+a = "Joao";
+a = false;
+a = [];
+let id = "10";
+id = 10;
+const userId = 10;
+const productId = "1010110";
+var Size;
+(function (Size) {
+    Size["P"] = "Pequeno";
+    Size["M"] = "Medio";
+    Size["G"] = "Grande";
+})(Size || (Size = {}));
+const camisa = {
+    name: "Camisa gola V",
+    size: Size.M,
+};
+console.log(camisa);
+let teste;
+teste = "autenticado";
+teste = null;
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(12, 12));
+function sayHelloTo(name) {
+    return `Hello, ${name}`;
+}
+console.log(sayHelloTo("Joao"));
+function logger(msg) {
+    console.log(msg);
+}
+logger("Teste!");
+function greeting(name, greet) {
+    greet ? console.log(`Olá ${greet} ${name}`) : console.log(`Olá ${name}`);
+}
+greeting("João");
+greeting("Carlos", "Sr.");
+function sumNumbers(nums) {
+    return nums.n1 + nums.n2;
+}
+console.log(sumNumbers({ n1: 1, n2: 6 }));
+function multiplyNumbers(nums) {
+    return nums.n1 * nums.n2;
+}
+let nums = {
+    n1: 5,
+    n2: 10,
+};
+console.log(multiplyNumbers(nums));
+function doSomething(info) {
+    if (typeof info === "number") {
+        console.log("O número é: " + info);
+        return;
+    }
+    console.log("Não foi passado um número");
+}
+doSomething(5);
+doSomething(true);
+function showArrayItems(arr) {
+    arr.map((item) => {
+        console.log(`ITEM: ${item}`);
+    });
+}
+showArrayItems([1, 56, 7]);
+showArrayItems(["s", "d", "g"]);
+class User {
+    name;
+    role;
+    isApproved;
+    constructor(name, role, isApproved) {
+        this.name = name;
+        this.role = role;
+        this.isApproved = isApproved;
+    }
+    showUserName() {
+        console.log(`O nome do usuario é: ${this.name}`);
+    }
+}
+const user1 = new User("Joao", "Admin", true);
+console.log(user1);
+user1.showUserName();
+class Car {
+    brand;
+    wheels;
+    constructor(brand, wheels) {
+        this.brand = brand;
+        this.wheels = wheels;
+    }
+    showBrand() {
+        console.log("A marca do carro é " + this.brand);
+    }
+}
+const c1 = new Car("VW", 4);
+c1.showBrand();
+class SuperCar extends Car {
+    engine;
+    constructor(brand, wheels, engine) {
+        super(brand, wheels);
+        this.engine = engine;
+    }
+}
+const c2 = new SuperCar("AUDI", 4, 2.0);
+console.log(c2);
+c2.showBrand();
+function baseParamters() {
+    return function (constructor) {
+        return class extends constructor {
+            id = Math.random();
+            createAt = new Date();
+        };
+    };
+}
+let Person = class Person {
+    name;
+    constructor(name) {
+        this.name = name;
+    }
+};
+Person = __decorate([
+    baseParamters()
+], Person);
+const p1 = new Person("Sam");
+console.log(p1);
 export {};
 //# sourceMappingURL=index.js.map
--- TypeScript/Matheus Battisti TS/src/index.ts
+++ TypeScript/Matheus Battisti TS/src/index.ts
@@ -3,3 +3,248 @@ let x: number = 10;
 x = 5;
 
 console.log(x);
+
+// inferencia x annotation
+let y = 20;
+
+// Tipos primitivos
+let nome: string = "Joao";
+let age: number = 21;
+const isAdmin: boolean = true;
+
+console.log(typeof nome);
+
+// object | array
+
+const myNumbers: number[] = [1, 2, 3];
+
+console.log(myNumbers);
+console.log(myNumbers.length);
+myNumbers.push(5);
+
+console.log(myNumbers);
+
+// tuplas -> tuple | dita como sera o array
+let myTupla: [number, string, string[]];
+
+myTupla = [5, "Joao", ["a", "b", "c"]];
+
+console.log(myTupla);
+
+// object literals -> {props: value}
+
+const user: { name: string; age: number } = {
+    name: "Joao",
+    age: 21,
+};
+
+console.log(user);
+console.log(user.name);
+
+// any
+let a: any = 0;
+
+a = "Joao";
+a = false;
+a = [];
+
+// union type
+let id: string | number = "10";
+
+id = 10;
+
+// id = true;
+// id = [];
+
+// type alias
+
+type myIdType = number | string;
+const userId: myIdType = 10;
+const productId: myIdType = "1010110";
+
+// enum
+enum Size {
+    P = "Pequeno",
+    M = "Medio",
+    G = "Grande",
+}
+
+const camisa = {
+    name: "Camisa gola V",
+    size: Size.M,
+};
+
+console.log(camisa);
+
+// literal types
+let teste: "autenticado" | null;
+
+teste = "autenticado";
+teste = null;
+
+// funções
+
+function sum(a: number, b: number) {
+    return a + b;
+}
+
+console.log(sum(12, 12));
+
+function sayHelloTo(name: string): string {
+    return `Hello, ${name}`;
+}
+
+console.log(sayHelloTo("Joao"));
+
+function logger(msg: string): void {
+    console.log(msg);
+}
+
+logger("Teste!");
+
+function greeting(name: string, greet?: string): void {
+    greet ? console.log(`Olá ${greet} ${name}`) : console.log(`Olá ${name}`);
+}
+
+greeting("João");
+
+greeting("Carlos", "Sr.");
+
+// interface
+
+interface MathFunctionParams {
+    n1: number;
+    n2: number;
+}
+
+function sumNumbers(nums: MathFunctionParams) {
+    return nums.n1 + nums.n2;
+}
+
+console.log(sumNumbers({ n1: 1, n2: 6 }));
+
+function multiplyNumbers(nums: MathFunctionParams) {
+    return nums.n1 * nums.n2;
+}
+
+let nums: MathFunctionParams = {
+    n1: 5,
+    n2: 10,
+};
+
+console.log(multiplyNumbers(nums));
+
+// narrowing | checagem de tipos
+// function greeting(name: string, greet?: string): void {
+//     greet ? console.log(`Olá ${greet} ${name}`) : console.log(`Olá ${name}`);
+// }
+
+function doSomething(info: number | boolean) {
+    if (typeof info === "number") {
+        console.log("O número é: " + info);
+        return;
+    }
+    console.log("Não foi passado um número");
+}
+
+doSomething(5);
+doSomething(true);
+
+// generics
+
+function showArrayItems<T>(arr: T[]) {
+    arr.map((item) => {
+        console.log(`ITEM: ${item}`);
+    });
+}
+
+showArrayItems([1, 56, 7]);
+showArrayItems(["s", "d", "g"]);
+
+// classes
+
+class User {
+    name;
+    role;
+    isApproved;
+
+    constructor(name: string, role: string, isApproved: boolean) {
+        this.name = name;
+        this.role = role;
+        this.isApproved = isApproved;
+    }
+
+    showUserName() {
+        console.log(`O nome do usuario é: ${this.name}`);
+    }
+}
+
+const user1 = new User("Joao", "Admin", true);
+
+console.log(user1);
+user1.showUserName();
+
+// interfaces em classes
+interface IVehicle {
+    brand: string;
+    showBrand(): void;
+}
+
+class Car implements IVehicle {
+    brand;
+    wheels;
+
+    constructor(brand: string, wheels: number) {
+        this.brand = brand;
+        this.wheels = wheels;
+    }
+
+    showBrand(): void {
+        console.log("A marca do carro é " + this.brand);
+    }
+}
+
+const c1 = new Car("VW", 4);
+
+c1.showBrand();
+
+// herança
+
+class SuperCar extends Car {
+    engine;
+
+    constructor(brand: string, wheels: number, engine: number) {
+        super(brand, wheels);
+        this.engine = engine;
+    }
+}
+
+const c2 = new SuperCar("AUDI", 4, 2.0);
+
+console.log(c2);
+
+c2.showBrand();
+
+// decorators
+// constructor decorator
+function baseParamters() {
+    return function <T extends { new (...args: any[]): {} }>(constructor: T) {
+        return class extends constructor {
+            id = Math.random();
+            createAt = new Date();
+        };
+    };
+}
+
+@baseParamters()
+class Person {
+    name;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+}
+
+const p1 = new Person("Sam");
+
+console.log(p1);
